77966abae3b2941cce5f116170fa50674e21680f
HAREESHRAVI <hareesh.unofficial@gmail.com>
2023-10-15 23:41:51 +0530

    partially implemented appear animation using js

--- Codes/javascript_codes/main.js
+++ Codes/javascript_codes/main.js
@@ -1,1 +1,12 @@
-prompt("Enter a number:");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+        } else {
+        entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => {observer.observe(el)});
